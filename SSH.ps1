2426147a9dcbb655fb850075b483d6127ae84039
Luiscolio243 <luiscolio243@gmail.com>
2025-02-26 18:33:15 -0700

    1
    3

--- SSH.ps1
+++ SSH.ps1
@@ -1,9 +1,12 @@
 
-
+#Instalación del Servidor OpenSSH
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
+#Iniciar el servicio SSHD
 Start-Service sshd 
 
+#Configurar el inicio automático del servicio
 Set-Service -Name sshd -StartupType 'Automatic'
 
+#Crear una regla de firewall para permitir conexiones SSH
 New-NetFirewallRule -Name "sshd" -DisplayName "OpenSSH Server (ssh)" -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
--- SSH.sh
+++ SSH.sh
@@ -1,15 +1,22 @@
 
+#Actualizar la lista de paquetes
 apt-get update
 
+#Instalar el servidor OpenSSH
 sudo apt -y install openssh-server
 
+#Habilitar el servicio SSH para que inicie automáticamente
 sudo systemctl enable ssh
 
+#Reiniciar el servicio SSH
 sudo systemctl restart ssh
 
+#Verificar el estado del servicio SSH
 sudo systemctl status ssh
 
+#Permitir conexiones SSH en el firewall
 sudo ufw allow ssh
 
+#Habilitar el firewall
 sudo ufw enable 
 
